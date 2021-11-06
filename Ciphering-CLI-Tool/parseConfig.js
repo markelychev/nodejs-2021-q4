40b74e058f91c30505fa526fdb18e26ed93d135b
A.Markelychev <a.markelychev@gmail.com>
2021-11-06 20:34:59 +0300

    feat: add checking repeat

--- Ciphering-CLI-Tool/parseConfig.js
+++ Ciphering-CLI-Tool/parseConfig.js
@@ -1,2 +1,15 @@
 const showErr = require ('./helpers/showErr');
 
+const optionsName = new Map([
+  ['-c --config', null],
+  ['-i --input', null],
+  ['-o --output', null]
+])
+
+const checkRepeat = (flagsStr, argsArr) => {
+  const arrFlags = flagsStr.split(' ');
+  const repats = arrFlags.reduce((sum, el) => {
+      return sum + argsArr.filter(item => item === el).length
+    }, 0)
+  return repats>1
+}
