34b8e7137b4500e71f615e17bd7492605ce6de5b
A.Markelychev <a.markelychev@gmail.com>
2021-11-08 23:20:39 +0300

    feat: add fn CreateLine and pipeline

--- Ciphering-CLI-Tool/index.js
+++ Ciphering-CLI-Tool/index.js
@@ -1,3 +1,31 @@
-const options = require ('./parseConfig');
+const { pipeline } = require('stream');
+const { stdin, stdout } = require('process');
+const options = require('./parseConfig');
+const ciphers = require('./ciphers/ciphers');
 
-console.log(options)
+let a = [];
+
+const createLine = (options) => {
+  const config = options.get('-c --config').split('-');
+  return config.map((item) => {
+    if (ciphers[item[0]] == undefined) {
+      throw new Error('Config Error');
+    }
+    return new ciphers[item[0]](+item[1]);
+  });
+};
+
+try {
+  a = createLine(options);
+} catch (err) {
+  process.stderr.write(err.message);
+  process.exit();
+}
+
+pipeline(stdin, ...a, stdout, (err) => {
+  if (err) {
+    console.error('Pipeline failed.', err.message);
+  } else {
+    console.log('Pipeline succeeded.');
+  }
+});
