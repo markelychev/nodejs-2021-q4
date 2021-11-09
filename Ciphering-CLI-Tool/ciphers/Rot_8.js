d72ef330ae037a01f60d4692b2ddfa040a166d9c
A.Markelychev <a.markelychev@gmail.com>
2021-11-10 00:07:04 +0300

    refactor: rename class

--- Ciphering-CLI-Tool/ciphers/Rot_8.js
+++ Ciphering-CLI-Tool/ciphers/Rot_8.js
@@ -1,10 +1,10 @@
 const ceaser = require('./CaeserCipher');
 
-class Rop_8 extends ceaser {
+class Rot_8 extends ceaser {
   constructor(options = 1) {
     super(options);
     this.shift = 8;
   }
 }
 
-module.exports = Rop_8;
+module.exports = Rot_8;
