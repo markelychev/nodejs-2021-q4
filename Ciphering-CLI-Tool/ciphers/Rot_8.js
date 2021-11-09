59127615789390cae9879dc23dfcd004c66cb54f
A.Markelychev <a.markelychev@gmail.com>
2021-11-10 00:08:24 +0300

    feat: add check config

--- Ciphering-CLI-Tool/ciphers/Rot_8.js
+++ Ciphering-CLI-Tool/ciphers/Rot_8.js
@@ -2,6 +2,9 @@ const ceaser = require('./CaeserCipher');
 
 class Rot_8 extends ceaser {
   constructor(options = 1) {
+    if (options < 0 || options > 1) {
+      throw new Error('Rot-8 cipher config error');
+    }
     super(options);
     this.shift = 8;
   }
