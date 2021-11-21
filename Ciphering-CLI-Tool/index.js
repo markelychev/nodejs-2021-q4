134e618fbfd41e870b18ef0b828f12e525e92815
A.Markelychev <a.markelychev@gmail.com>
2021-11-21 12:29:32 +0300

    feat: add WriteStream class

--- Ciphering-CLI-Tool/index.js
+++ Ciphering-CLI-Tool/index.js
@@ -1,9 +1,11 @@
 const { pipeline } = require('stream');
 const { stdin, stdout } = require('process');
+const WriteStream = require('./WriteStream/WriteStream');
 const options = require('./parseConfig');
 const ciphers = require('./ciphers/ciphers');
+const ReadStream = require('./ReadStream/ReadStream');
 
-let a = [];
+let streams = [];
 
 const createLine = (options) => {
   const MAX_CONF_ITEM_LENGTH = 2;
@@ -22,13 +24,17 @@ const createLine = (options) => {
 };
 
 try {
-  a = createLine(options);
+  const rStream = options.get('-i --input') ? new ReadStream(options.get('-i --input')) : stdin;
+  const wStream = options.get('-o --output') ? new WriteStream(options.get('-o --output')) : stdout;
+  streams.push(rStream);
+  streams = streams.concat(createLine(options));
+  streams.push(wStream);
 } catch (err) {
   process.stderr.write(err.message);
   process.exit();
 }
 
-pipeline(stdin, ...a, stdout, (err) => {
+pipeline(...streams, (err) => {
   if (err) {
     console.error('Pipeline failed.', err.message);
   } else {
