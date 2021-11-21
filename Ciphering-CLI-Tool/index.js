eb8780e688aa1603a89003bbc6ce1b3234e6272b
A.Markelychev <a.markelychev@gmail.com>
2021-11-21 22:05:13 +0300

    refactor: refactor parser

--- Ciphering-CLI-Tool/index.js
+++ Ciphering-CLI-Tool/index.js
@@ -1,13 +1,14 @@
 const { pipeline } = require('stream');
 const { stdin, stdout } = require('process');
 const WriteStream = require('./WriteStream/WriteStream');
-const options = require('./parseConfig');
+const parseConfig = require('./parseConfig');
 const ReadStream = require('./ReadStream/ReadStream');
 const createStreamsLine = require('./createStreamsLine/createStreamsLine');
 
 let streams = [];
 
 try {
+  const options = parseConfig(process.argv);
   const rStream = options.get('-i --input') ? new ReadStream(options.get('-i --input')) : stdin;
   const wStream = options.get('-o --output') ? new WriteStream(options.get('-o --output')) : stdout;
   streams.push(rStream);
